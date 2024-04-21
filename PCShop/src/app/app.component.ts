e1fc733cc32a841d6860a59edd215593491524ee
Anuar Adilbek <adilbek.anuar@gmail.com>
2024-04-21 21:38:32 +0500

    added top-bar, initialized Django project

--- PCShop/src/app/app.component.ts
+++ PCShop/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { HomeComponent } from './home/home.component';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CartComponent } from './cart/cart.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, TopBarComponent, ProductDetailComponent, ProductListComponent, HomeComponent, CategoryListComponent, CartComponent, AuthorizationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- PCShop/src/app/app.routes.ts
+++ PCShop/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'products', component: ProductListComponent},
+    {path: 'auth', component: AuthorizationComponent},
+    {path: 'cart', component: CartComponent},
+    {path: 'products/:id', component: ProductDetailComponent}
+];
