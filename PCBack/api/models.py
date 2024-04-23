fc310bd9334dfe9b5f755207735de46d63ad89e0
Anuar Adilbek <adilbek.anuar@gmail.com>
2024-04-23 20:27:30 +0500

    django models

---
+++ PCBack/api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField()
+    image = models.ImageField()
+    category_id = models.ForeignKey(to='Category', on_delete=models.CASCADE)
